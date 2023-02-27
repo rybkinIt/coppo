889beaeddadeb3953757250ff051e8ca3ec648f8
rybkinIt <116676756+rybkinIt@users.noreply.github.com>
2023-02-27 22:06:38 +0300

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,7 @@
 const Header = () =>{
     return(
-        <div className="header-container">
-            <div className="header-logo">//</div>
-            <div className="header-percent">///</div>
+        <div>
+            
         </div>
     )
 }
